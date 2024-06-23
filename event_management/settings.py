04e78d9b465fe9b752e1b03fd7650579ad5c2a6b
ashiqaq21 <138993380+ashiqaq21@users.noreply.github.com>
2024-06-23 16:07:13 +0530

    settings.py

--- event_management/settings.py
+++ event_management/settings.py
@@ -111,8 +111,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.your-email-provider.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'your-email@example.com'
-EMAIL_HOST_PASSWORD = 'your-email-password'
+EMAIL_HOST_USER = 'eventm385@example.com'
+EMAIL_HOST_PASSWORD = 'pkik rjkd mywp gonj'
 
 
 # settings.py
